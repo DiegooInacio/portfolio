efe0a1a88e8059a4af4b94075c725c614cafdef4
DiegooInacio <diego.inacio07@aluno.ifce.edu.br>
2026-09-12 13:10:19 -0300

    tema claro por padrão

--- src/App.tsx
+++ src/App.tsx
@@ -243,7 +243,7 @@ export function App() {
       return storedTheme;
     }
 
-    return window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+    return "light";
   });
 
   useEffect(() => {
